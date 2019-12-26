1d611f244903003c03929c09d4b0b6a5e0342181
urdnot <urdnot@yandex.by>
2019-12-26 20:51:08 +0300

    bitset for choise base complete

---
+++ doorlose/database/test/detail/bitset_test.cpp
@@ -0,0 +1,61 @@
+﻿#include <doorlose/database/detail/bitset.hpp>
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <bitset>
+#include <vector>
+
+namespace doorlose {
+namespace database {
+namespace detail {
+namespace {
+
+class bitset_test : public ::testing::Test
+{
+public:
+    bitset_test()
+    {
+    }
+};
+
+TEST_F(bitset_test, basic)
+{
+    std::vector<std::uint64_t> buf(4);
+    std::vector<std::uint64_t> generated;
+    const std::uint64_t bit_size = buf.size() * 64 - 13;
+
+    for (std::uint64_t i = 0; i < bit_size; ++i)
+    {
+        const std::uint64_t num =
+            bitset::get_random(reinterpret_cast<std::uint8_t *>(buf.data()), bit_size);
+
+        const auto begin = generated.begin();
+        const auto end = generated.end();
+        if (std::find(begin, end, num) != end)
+        {
+            FAIL();
+            break;
+        }
+        else
+        {
+            generated.push_back(num);
+        }
+    }
+
+    EXPECT_EQ(0xFFFFFFFFFFFFFFFF, buf[0]);
+    EXPECT_EQ(0xFFFFFFFFFFFFFFFF, buf[1]);
+    EXPECT_EQ(0xFFFFFFFFFFFFFFFF, buf[2]);
+    EXPECT_EQ(0xFFFFFFFFFFFFE000, buf[3]);
+
+    bitset::get_random(reinterpret_cast<std::uint8_t *>(buf.data()), bit_size);
+
+    const auto result = std::bitset<64>(buf[0]).count() + std::bitset<64>(buf[1]).count() +
+        std::bitset<64>(buf[2]).count() + std::bitset<64>(buf[3]).count();
+    EXPECT_EQ(1, result);
+}
+
+} // namespace
+} // namespace detail
+} // namespace database
+} // namespace doorlose

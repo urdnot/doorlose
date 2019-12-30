6fc47c77eb3fffda48d699ce88f750682914aaf1
urdnot <urdnot@yandex.by>
2019-12-30 22:45:02 +0300

    database interface

--- doorlose/database/include/doorlose/database/database.hpp
+++ doorlose/database/include/doorlose/database/database.hpp
@@ -10,8 +10,62 @@ namespace database {
 class DOORLOSE_DATABASE_EXPORT database
 {
 public:
+    struct get_request_t
+    {
+        std::uint64_t client_id;
+        std::uint64_t group_id;
+    };
+
+    struct get_response_t
+    {
+        std::uint64_t client_id;
+        std::uint64_t group_id;
+        std::string_view task;
+    };
+
+public:
     explicit database();
 
+    //explicit database();
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Client
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Get random task from specified group for specified client
+     */
+    get_response_t get_task(get_request_t req);
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Administration
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     *
+     */
+    std::string_view examine_task(std::uint64_t group_id, std::uint64_t task_id);
+
+    /**
+     *
+     */
+    void update_task(std::uint64_t group_id, std::uint64_t task_id, std::string_view task);
+
+    /**
+     *
+     */
+    void remove_task(std::uint64_t group_id, std::uint64_t task_id);
+
+    /**
+     * Add tasks from UTF-8 json file
+     */
+    void add_from(const std::filesystem::path &from);
+
+    /**
+     * Replace tasks from UTF-8 json file, wipe all clients choises
+     */
+    void replace_from(const std::filesystem::path &from);
 
     void serialize(const std::filesystem::path &to_folder) const;
     void deserialize(const std::filesystem::path &from_folder);
@@ -19,5 +73,5 @@ public:
 private:
 };
 
-}
-}
+} // namespace database
+} // namespace doorlose

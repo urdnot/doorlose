46e2bb6c2dbc1d1561560a05c5997ad4f38a5366
urdnot <urdnot@yandex.by>
2020-01-22 11:48:36 +0300

    databse some unit tests added

--- backend/addon/database/test/database_test.cpp
+++ backend/addon/database/test/database_test.cpp
@@ -55,6 +55,46 @@ TEST_F(database_test, get_task_from_one_variant)
     EXPECT_EQ(TEST_TASK, res2.second);
 }
 
+TEST_F(database_test, get_task_except_removed)
+{
+    database db;
+    db.add_task(0, TEST_TASK);
+    db.add_task(0, TEST_TASK);
+    db.add_task(0, TEST_TASK_1);
+    db.update_task(0, 0, true);
+    db.update_task(0, 1, true);
+
+    const auto res = db.get_task(database::UNDEFINED_CLIENT_ID, 0);
+    EXPECT_EQ(TEST_TASK_1, res.second);
+    const auto res1 = db.get_task(res.first, 0);
+    EXPECT_EQ(TEST_TASK_1, res1.second);
+    const auto res2 = db.get_task(res.first, 0);
+    EXPECT_EQ(TEST_TASK_1, res2.second);
+}
+
+TEST_F(database_test, examine_task_group_id_out_of_range)
+{
+    database db;
+    db.add_task(0, TEST_TASK);
+    EXPECT_THROW(db.examine_task(10, 0), invalid_argument);
+}
+
+TEST_F(database_test, examine_task_check_all_cases)
+{
+    database db;
+    db.add_task(0, TEST_TASK);
+    db.update_task(0, 0, true);
+    db.add_task(0, TEST_TASK_1);
+
+    const auto res = db.examine_task(0, 0);
+    EXPECT_EQ(TEST_TASK, res.first);
+    EXPECT_TRUE(res.second);
+
+    const auto res1 = db.examine_task(0, 1);
+    EXPECT_EQ(TEST_TASK_1, res1.first);
+    EXPECT_FALSE(res1.second);
+}
+
 } // namespace
 } // namespace database
 } // namespace addon
